446aaa0166e6677966518c1382ef440ba6af11db
gagan1228 <146916437+gagan1228@users.noreply.github.com>
2024-01-20 02:02:30 +0530

    Add files via upload

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -3,6 +3,7 @@ const vendorController=require('./../controllers/vendorController')
 const router=express.Router();
 
 router.route('/signup').post(vendorController.createVendor)
+router.route('/').get(vendorController.getallvendors)
 //router.route('/newuserverify').post(vendorController.vendorVerify)
 
 
