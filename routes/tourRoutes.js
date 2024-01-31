c83b17f80be49385f1be52470bdcb6b4241056aa
gagan1228 <146916437+gagan1228@users.noreply.github.com>
2024-01-31 23:42:54 +0530

    Add files via upload

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,10 @@
 const express=require("express")
 const tourController=require('./../controllers/tourController')
+const vendorAuthController=require('./../controllers/vendorController')
 const router=express.Router();
 router.route('/top-2-tours').get(tourController.aliasTopTours , tourController.getAllTours);
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour);
+router.route('/').get(vendorAuthController.protect,tourController.getAllTours).post(tourController.createTour);
+router.route('/:id').get(vendorAuthController.protect, vendorAuthController.restrictTo('admin'),tourController.getTour).patch(tourController.updateTour);
 
 module.exports=router;
 
--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -3,9 +3,14 @@ const vendorController=require('./../controllers/vendorController')
 const router=express.Router();
 
 router.route('/signup').post(vendorController.createVendor)
+router.route('/verifysignup').post(vendorController.signupvendor)
 router.route('/').get(vendorController.getallvendors)
+router.route('/login').post(vendorController.login)
 //router.route('/newuserverify').post(vendorController.vendorVerify)
-
-
+router.route('/forgotpassword').post(vendorController.forgotPassword)
+router.route('/resetpassword/:token').post(vendorController.ResetPassword)
+router.route('/updateMyPassword').patch(vendorController.protect,vendorController.updatePassword)
+router.route('/updateMe').patch(vendorController.protect,vendorController.updateMe)
+router.route('/deletevendor').delete(vendorController.protect,vendorController.deletevendor)
 module.exports=router;
 
