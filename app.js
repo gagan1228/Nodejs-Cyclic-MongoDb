233a8c74265aa4ad972d3fa6868adcacaa69b444
gagan1228 <146916437+gagan1228@users.noreply.github.com>
2024-02-15 13:53:24 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const rateLimit=require('express-rate-limit')
 const cors=require('cors')
 const AppError=require('./utils/appError')
 const globalErrorHandler=require('./controllers/errorController')
+const gparentRouter=require('././routes/gparentcategoryroutes')
 const tourRouter=require('./routes/tourRoutes')
 const categoryRouter=require('./routes/categoryRoutes')
 const subcategoryRouter=require('./routes/subcategoryRoutes')
 const vendorRouter=require('./routes/vendorRoutes')
 const userRouter=require('./routes/userRoutes')
 const app=express();
-app.use(cors())
+//app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.static(`${__dirname}/public`))
@@ -43,6 +44,7 @@ app.use('/api/v1/category',categoryRouter)
 app.use('/api/v1/subcat',subcategoryRouter)
 app.use('/api/v1/vendor',vendorRouter)
 app.use('/api/v1/user',userRouter)
+app.use('/api/v1/gparentcat',gparentRouter)
 
 app.all('*',(req,res,next)=>{
     res.status(400).json({
