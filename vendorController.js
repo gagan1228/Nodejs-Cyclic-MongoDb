b6ac30541e9326c2bdc802c6dd5c2e6378396425
gagan1228 <146916437+gagan1228@users.noreply.github.com>
2024-02-16 14:07:10 +0530

    Add files via upload

--- vendorController.js
+++ vendorController.js
@@ -132,7 +132,7 @@ exports.createVendor=catchAsync(async(req,res,next)=>{
 
 )
 exports.signupvendor=catchAsync(async(req,res,next)=>{
-    if(req.body.otp.equals(req.body.userotp))
+    if(req.body.isverified)
     {
         const newVendor=await Vendor.create(req.body);
             const token=signToken(Vendor._id)
