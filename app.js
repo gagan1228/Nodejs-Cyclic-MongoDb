1baf253ab0c76be41da69994eef965e31a40dda8
gagan1228 <146916437+gagan1228@users.noreply.github.com>
2024-02-17 15:31:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const subcategoryRouter=require('./routes/subcategoryRoutes')
 const vendorRouter=require('./routes/vendorRoutes')
 const userRouter=require('./routes/userRoutes')
 const app=express();
-app.use(cors())
+app.use(cors(
+    {origin: "http://localhost:5173",
+    methods : ["GET", "POST", "PUT", "DELETE"]
+})
+)
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
